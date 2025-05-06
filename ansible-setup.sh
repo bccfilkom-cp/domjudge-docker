4abbebb36e625389f875e9d099f37f326ce81e37
devanfer <ferrelvsc20@gmail.com>
2025-05-06 18:52:27 +0700

    refactor(ansible): refactor ansible code yamls structure

--- ansible-setup.sh
+++ ansible-setup.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
 # make sure variables in inventory are set up correctly
-
+cd ansible 
 echo 'Checking remote server...';
-if [ -z "$(ansible vps -m ping | grep SUCCESS)" ]; then 
-    echo "Failed to ping to remote server"
-    echo "Exiting..."
-    exit 1
+if [ -z "$(ansible all -m ping | grep SUCCESS)" ]; then 
+    echo "Failed to ping to remote server";
+    echo "Exiting...";
+    exit 1;
 fi 
 
-echo 'Remote server ready!'
+echo -e "\033[1;35m>>> REMOTE SERVER READY! <<<\033[0m"
+echo -e "\033[1;36m>>> Starting Provision Playbook <<<\033[0m"
+ansible-playbook playbooks/provision.yml
 
-echo 'Executing playbook docker-instl';
-ansible-playbook ansible/playbooks/00-docker-instl.yml 
-
-echo 'Executing playbook enable-cgroup';
-ansible-playbook ansible/playbooks/01-enable-cgroup.yml
-
-echo 'Executing playbook clone-deploy';
-ansible-playbook ansible/playbooks/02-clone-deploy.yml -v | tee >(grep "New password for admin")
+echo -e "\033[1;36m>>> Starting Deploy Playbook <<<\033[0m"
+ansible-playbook -e @inventory/vars.yaml playbooks/deploy.yml -v | tee >(grep "New password for admin")
